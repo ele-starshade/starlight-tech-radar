11e3abff6b1bdc603e18465678e9e60a5f1b36a9
ele.starshade <ele.starshade@gmail.com>
2026-03-02 19:57:21 +0000

    chore: fix netlify deployment

--- netlify/functions/server.cjs
+++ netlify/functions/server.cjs
@@ -1,11 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
+const path = require('node:path')
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const serverless = require('serverless-http')
 
 let handler
 
 module.exports.handler = async (event, context) => {
   if (!handler) {
-    const { app } = await import('../../dist/ssr/index.js')
+    // In Netlify functions, process.cwd() is the root of the project.
+    // We import the built SSR app dynamically.
+    const appPath = path.resolve(process.cwd(), 'dist/ssr/index.js')
+    const { app } = await import(appPath)
 
     handler = serverless(app)
   }
